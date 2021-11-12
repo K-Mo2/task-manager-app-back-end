f744d5eefb19abe9a4b65acfeb2cdb3f21ee9b2b
K-Mo2 <karimelsisi27@gmail.com>
2021-11-12 22:00:11 +0200

    creating Schemas with mongoose and saving them async

--- mongoose.js
+++ mongoose.js
@@ -4,4 +4,18 @@ const url = "mongodb://localhost:27017/test";
 
 async function main() {
   await mongoose.connect(url);
+
+  const taskSchema = new mongoose.Schema({
+    description: String,
+    completed: Boolean,
+  });
+
+  const Task = mongoose.model("Task", taskSchema);
+
+  const task1 = new Task({ description: "Going home", completed: true });
+  return await task1.save();
 }
+
+main()
+  .then((task1) => console.log(task1))
+  .catch(() => console.error);
