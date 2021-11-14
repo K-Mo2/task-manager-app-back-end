76fc4196fb9d816097e6852d145c9404c300d04c
K-Mo2 <karimelsisi27@gmail.com>
2021-11-14 14:04:21 +0200

    adding get requests to database to fetch users

--- src/database/mongoose.js
+++ src/database/mongoose.js
@@ -3,14 +3,16 @@ const userSchema = require("../models/users");
 
 const url = "mongodb://localhost:27017/test";
 
-async function main(schemaName,schema, data) {
+async function main(schemaName, schema, data) {
   await mongoose.connect(url);
 
   const User = mongoose.model(`${schemaName}`, schema);
-
+  if (!data) {
+    return User;
+  }
   const dataInstance = new User(data);
 
-  return dataInstance.save();
+  return dataInstance;
 }
 
 module.exports = { main };
--- src/index.js
+++ src/index.js
@@ -13,14 +13,38 @@ app.listen(port, () => {
 
 app.use(express.json());
 
+app.get("/users", (req, res) => {
+  main("users", userSchema).then((data) => {
+    data
+      .find({})
+      .then((result) => {
+        if (!result) {
+          res.status(500).send("Error");
+        } else {
+          res.status(201).send(result);
+        }
+      })
+      .catch((error) => {
+        res.status(500).send(error);
+      });
+    throw new Error("Error:failed to fetch");
+  });
+});
+
 app.post("/users", (req, res) => {
   main("users", userSchema, req.body)
-    .then((data) => res.status(201).send(data))
+    .then((data) => {
+      data.save();
+      res.status(201).send(data);
+    })
     .catch((error) => res.status(400).send(error));
 });
 
 app.post("/tasks", (req, res) => {
   main("tasks", taskSchema, req.body)
-    .then((data) => res.status(201).send(data))
+    .then((data) => {
+      data.save();
+      res.status(201).send(data);
+    })
     .catch((error) => res.status(400).send(error));
 });
