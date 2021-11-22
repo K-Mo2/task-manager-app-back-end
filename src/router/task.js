1d899124c7cb69d13534d8fb0d2c86f65d7fb88b
K-Mo2 <karimelsisi27@gmail.com>
2021-11-22 14:23:09 +0200

    adding authentication to all the requests in task router

--- src/router/task.js
+++ src/router/task.js
@@ -5,7 +5,7 @@ const auth = require("../middleware/auth");
 
 const router = new express.Router();
 
-router.get("/tasks/me", auth, (req, res) => {
+router.get("/tasks", auth, (req, res) => {
   main("tasks", taskSchema).then((data) => {
     data.find({ owner: req.user._id }).then((result) => {
       if (!result) {
@@ -25,7 +25,7 @@ router.post("/tasks", auth, (req, res) => {
     .catch((error) => res.status(400).send(error));
 });
 
-router.patch("/tasks/:id", async (req, res) => {
+router.patch("/tasks/:id", auth, async (req, res) => {
   try {
     const updates = Object.keys(req.body);
     const allowedUpdates = ["task", "completed"];
@@ -38,13 +38,17 @@ router.patch("/tasks/:id", async (req, res) => {
 
     const id = req.params.id;
     const modelInstance = await main("tasks", taskSchema);
-    const result = await modelInstance.findById(id);
-    updates.forEach((update) => (result[update] = req.body[update]));
-    await result.save();
+    const result = await modelInstance.findOne({
+      _id: id,
+      owner: req.user._id,
+    });
 
     if (!result) {
       res.status(404).send(new Error("Not Found"));
     }
+
+    updates.forEach((update) => (result[update] = req.body[update]));
+    await result.save();
 
     res.status(201).send(result);
   } catch (error) {
@@ -52,10 +56,13 @@ router.patch("/tasks/:id", async (req, res) => {
   }
 });
 
-router.delete("/tasks", async (req, res) => {
+router.delete("/tasks/:id", auth, async (req, res) => {
   try {
     const modelInstance = await main("tasks", taskSchema);
-    const result = await modelInstance.findOneAndDelete(req.body);
+    const result = await modelInstance.findOneAndDelete({
+      _id: req.params.id,
+      owner: req.user._id,
+    });
     !result ? new Error() : res.status(201).send(result);
   } catch (error) {
     res.status(404).send(error);
